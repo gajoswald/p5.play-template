30be42edb3fb140e5314159b34204e1e564c6520
Mr. Oswald <62300716+gajoswald@users.noreply.github.com>
2024-04-04 12:55:19 +0000

    Updated versions and changed default code.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # p5play
 
-This is an update to the default template Repl for p5.js. The version of p5 has been updated to 1.6 and the sound library has also been included. Also included is p5play and planck. 
+This is an update to the default template Repl for p5.js. The version of p5 has been updated to 1.9.1 and the sound library has also been included. Also included is p5play and planck. 
 
 p5.js is a JavaScript library for creative coding, with a focus on making coding accessible and inclusive for artists, designers, educators, beginners, and anyone else! p5.js is free and open-source because we believe software, and the tools to learn it, should be accessible to everyone. [Check out the official docs here](https://p5js.org/reference/).
 
--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-let colorlist = ['gold', 'yellow', 'turquoise', 'red']
+// https://sashamaps.net/docs/resources/20-colors/
+let colorlist = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000']
+
+let sprite
+let floor 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(255);
+  createCanvas(windowWidth, windowHeight)
+  background(255)
+
+  world.gravity.y = 10
+  
+  sprite = new Sprite()
+  sprite.diameter = 50
+  sprite.color = random(colorlist)
+  sprite.textSize = 30
+  sprite.text = "p5"
+
+  floor = new Sprite()
+  floor.color = random(colorlist)
+  floor.y = height-10;
+  floor.w = width;
+  floor.h = 5;
+  floor.collider = 'k';
+  floor.velocity.y = -1
 }
 
 function draw() {
-  noStroke()
-  fill(random(colorlist));
-  ellipse(mouseX, mouseY, 25, 25);
+  clear()  
+  const theta = atan2(mouseY-sprite.y, mouseX-sprite.x)
+  console.log( theta )
+  sprite.rotation = theta
+
+  if( floor.y < sprite.diameter ) { floor.y = height - 10 }
+  if( floor.y > height - 10 ) { floor.y = height - 10 }
 }
